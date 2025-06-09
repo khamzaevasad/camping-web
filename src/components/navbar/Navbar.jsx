988da12ff215082085503c637af87cd9b17e700f
Asadbek Xamzayev <xamzayevasad442@gmail.com>
2025-06-09 21:14:20 +0900

    create nav section

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import './Navbar.css';
+import navBottomIcon from '../assets/navbottom.svg'; // svg faylingiz src/components/navbar ichida bo'lishi kerak
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="nav-left">
+          <h3 className="logo">Camper</h3>
+          <div className="nav-item active">
+            Motor <img src={navBottomIcon} alt="dropdown" />
+          </div>
+          <div className="nav-item">
+            Caravan <img src={navBottomIcon} alt="dropdown" />
+          </div>
+          <div className="nav-item">
+            Tuning <img src={navBottomIcon} alt="dropdown" />
+          </div>
+          <div className="nav-item">
+            Used Car <img src={navBottomIcon} alt="dropdown" />
+          </div>
+          <div className="nav-item">Camping Place</div>
+        </div>
+
+        <div className="nav-right">
+          <FaShoppingCart className="icon" />
+          <FaUser className="icon" />
+          <div className="nav-item">
+            En <img src={navBottomIcon} alt="dropdown" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
